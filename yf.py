3ce52c40b470e4c4ced8d2893d922b24d2314ede
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-03-02 17:56:23 -0300

    Add files via upload

---
+++ yf.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 17:37:00 2023
+
+@author: USUARIO
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+import streamlit as st
+
+df = yf.download("TSLA")
+
+st.header('''
+            TESLA'S STOCK
+          ''')
+st.write("Tesla adj close: ", df['Adj Close'])
+
+st.line_chart(df['Adj Close'])
